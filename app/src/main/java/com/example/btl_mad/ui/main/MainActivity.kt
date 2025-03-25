e2388254858fabd202e684b80b6371456a38f9d5
buithuctuan <buithuctuan1507@gmail.com>
2025-03-25 21:01:28 +0700

    change folder tree

--- app/src/main/java/com/example/btl_mad/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/btl_mad/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.btl_mad
+package com.example.btl_mad.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.btl_mad.R
 import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
