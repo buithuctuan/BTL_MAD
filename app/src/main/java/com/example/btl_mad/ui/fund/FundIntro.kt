6789b47c558ccf599d78a9096bacad676a1571c6
hoanganh <lesyhoanganh2503@gmail.com>
2025-04-23 10:18:33 +0700

    complete4

--- app/src/main/java/com/example/btl_mad/ui/fund/FundIntro.kt
+++ app/src/main/java/com/example/btl_mad/ui/fund/FundIntro.kt
@@ -3,21 +3,32 @@ package com.example.btl_mad.ui.fund
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.btl_mad.R
+import com.example.btl_mad.ui.home.HomeFragment
+import com.example.btl_mad.ui.main.MainActivity
 
 class FundIntro : AppCompatActivity() {
+    private lateinit var navHome: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.fund_intro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            insets
+
+        navHome = findViewById(R.id.nav_home)
+        navHome.setOnClickListener {
+            navigateToHome()
         }
+
+    }
+    fun navigateToHome() {
+        val intent = Intent(this@FundIntro, MainActivity::class.java)
+        startActivity(intent)
     }
 
     fun goListFund(view: View?) {
